fbb40f7aaf45807457507a0f6f8fa11491f07191
AkinyiOwuor <owuorrachel@gmail.com>
2020-05-04 21:00:18 +0300

    initialize app

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_option[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Will add the views and forms
+
+    return app

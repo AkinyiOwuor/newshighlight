68c846d47e2d5bfd59f6aa3942b3080cbd5895e4
AkinyiOwuor <owuorrachel@gmail.com>
2020-05-04 21:25:47 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_option
+
+bootstrap = Bootstrap()
+
 def create_app(config_name):
 
     app = Flask(__name__)

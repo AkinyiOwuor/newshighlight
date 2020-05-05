2c260a17a8cca1c308d710643fb445728bda6b32
AkinyiOwuor <owuorrachel@gmail.com>
2020-05-05 10:21:25 +0300

    delete some arguments

--- app/tests/test_article.py
+++ app/tests/test_article.py
@@ -10,7 +10,7 @@ class ArticleTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_article = Article('independent','Independent','US and China clashing at sea is only a matter of time','Defence secretaries Jim Mattis and Wei Fenghe expected to discuss conflict between two countries','https://www.independent.co.uk/news/world/asia/us-china-south-sea-navy-war-ship-tension-jim-mattis-wei-fenghe-a8625676.html','https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/11/09/10/south-china-sea.jpg')
+        self.new_article = Article('independent','Independent','https://www.independent.co.uk/news/world/asia/us-china-south-sea-navy-war-ship-tension-jim-mattis-wei-fenghe-a8625676.html','https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/11/09/10/south-china-sea.jpg')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Article))
